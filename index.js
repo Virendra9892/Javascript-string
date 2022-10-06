883400b2ea87d4960466399d41352d0c38ff90e9
Virendra9892 <virendrap9892@gmail.com>
2022-10-06 18:42:43 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+let str = "Virendra Pathak";
+console.log(str);
+// console.log(str.length);
+console.log(str.indexOf("a"));
+console.log(str.lastIndexOf("a"));
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str.concat(" Is Good Boy."));
+console.log(str.slice(2,5));
+console.log(str.substring(2,5));
+// console.log(str.substr(2,5));
+console.log(str.charAt(0));
+console.log(str.charCodeAt(2));
+console.log(str.replace("Pathak","Golu"));
+// console.log(str.)
+let names = "Virendra";
+for(let i = 0; i<names.length; i++){
+    console.log(names[i]);
+}
+console.log(names.includes("p"));
+console.log(names.startsWith("Vire"));
+console.log(names.endsWith("Dra"));
